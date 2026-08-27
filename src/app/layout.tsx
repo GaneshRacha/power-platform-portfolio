74e0e5f3c2ac575e8cceb5399d48356fc639c066
GaneshRacha <60355360+GaneshRacha@users.noreply.github.com>
2026-08-27 15:07:29 -0400

    Revert portfolio theme import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./profile.css";
-import "./studio-theme.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { AmbientEffects } from "@/components/AmbientEffects";
