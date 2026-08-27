b76cac977bd89bdebc232363d1a89f6bb07d73c5
GaneshRacha <60355360+GaneshRacha@users.noreply.github.com>
2026-08-27 14:59:19 -0400

    Activate colorful studio theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./profile.css";
+import "./studio-theme.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { AmbientEffects } from "@/components/AmbientEffects";
